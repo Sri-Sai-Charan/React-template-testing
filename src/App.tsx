e637cb64573846614018e0188290e75a06b50421
Sri-Sai-Charan <svellise@umd.edu>
2022-10-18 18:36:28 -0400

    Uploading the project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import './App.css';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />      
+          
+        
+    </div>
+  );
+}
+
+export default App;
